08837f234daa6b7d5d25d918af022d254fc3636f
dariakorsunart <136754269+dariakorsunart@users.noreply.github.com>
2023-09-07 12:59:07 -0400

    meditation app first commit

---
+++ ocean/ocean.js
@@ -0,0 +1,39 @@
+const startButton = document.querySelector("#startButton");
+
+
+
+document.querySelector('#startButton').addEventListener('click',()=>{
+    //document.querySelector('#player').play()
+    document.querySelector('#player').muted=false
+}
+)
+
+const timer = 3;
+let amountTime = timer * 60;
+
+function calculateTime() {
+    let countdown = document.querySelector("#countdown");
+    let minutes = Math.floor(amountTime/60);
+    let seconds = amountTime%60;
+
+
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+
+    countdown.textContent = `${minutes} : ${seconds}`;
+    amountTime--;
+
+    console.log(amountTime);
+
+    if (amountTime < 0){
+        stopTimer();
+        amountTime = 0;
+    }
+
+    function stopTimer(){
+        clearInterval(timerId);
+    }
+}
+
+let timerId = setInterval(calculateTime, 1000);
